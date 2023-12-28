be5e41408ecbb7832507961d8937a65bec28f243
erikmacinnis <erikhyun35@gmail.com>
2023-12-28 11:56:36 -0800

    feat: receiving inputs from user

--- Blog.Web/Controllers/AdminTagsController.cs
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
@@ -10,5 +11,28 @@ namespace Blog.Web.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+
+            return View("Add");
+        }
+
+        // Can be replaced with this method
+        //[HttpPost]
+        //// Will make the action visible to AdminTags/Add url so we can use it in the add.cshtml page
+        //[ActionName("Add")]
+        //public IActionResult SubmitTag()
+        //{
+        //    // This must match the name params in the form
+        //    //var name = Request.Form["name"];
+        //    // var displayName = Request.Form["displayName"];
+
+        //    // Necessary because function name doesn't match a view
+        //    return View("Add");
+        //}
     }
 }
