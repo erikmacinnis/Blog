80ea03c4848efded74c0fb21614b89259fb932ab
erikmacinnis <erikhyun35@gmail.com>
2023-12-28 10:35:35 -0800

    feat: started adminTags

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        // Get method for /AdminTags/Add path
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
