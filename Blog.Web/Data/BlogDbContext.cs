a93261cdd9014d383fcbde8dec857d953edcc6e6
erikmacinnis <erikhyun35@gmail.com>
2023-12-28 06:49:02 -0800

    Add project files.

---
+++ Blog.Web/Data/BlogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        // Uses the BlogPosts name to create the table in the db
+        // DbSet is a table of given type
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
