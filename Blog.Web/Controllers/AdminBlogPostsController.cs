98fca6b4f50b7c60aae0d64227ff646d5a035ff1
erikmacinnis <erikhyun35@gmail.com>
2023-12-29 07:29:09 -0800

    feat: added add blog post option

---
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,40 @@
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository tagRepository;
+        public AdminBlogPostsController(ITagRepository tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+        public async Task<IActionResult> Add()
+        {
+            var tags = await tagRepository.GetAllAsync();
+
+            // Setting the available tags in the AddBlogPostRequest
+            var model = new AddBlogPostRequest
+            {
+                // Creating the SelectedListItem
+                // Select transforms them into a selected list item
+                Tags = tags.Select(x => new SelectListItem
+                {
+                    Text = x.DisplayName,
+                    // Using a unique id
+                    Value = x.Id.ToString(),
+                })
+            };
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            return RedirectToAction("Add");
+        }
+    }
+}
